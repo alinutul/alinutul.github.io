4d6198fef0fa39281ba5d1569117e72adbd5da70
Ulici Alin <77897341+alinutul@users.noreply.github.com>
2024-03-07 19:18:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function Button1(){
+    var paragraph = document.getElementById("demo1");
+    paragraph.textContent = "In aceste momente, primele convoaie aliate incep sa paraseasca coasta de sud a Angliei. Unele nave au fost nevoite sa porneasca atat de devreme deoarece aveau o viteza redusa si o distanta mare de parcurs";
+}
+function Button2(){
+    var paragraph = document.getElementById("demo2");
+    paragraph.textContent = "Parasutistii au inceput plecarea din bazele Britanice, fiind primii care o sa ajunga in Normandia.";
+}
+function Button3(){
+    var paragraph = document.getElementById("demo3");
+    paragraph.textContent = "Bombardamentele navale incep, iar soldatii naufragiaza in plajele Utah si Omaha.";
+    
+}
+function Button4(){
+    var paragraph = document.getElementById("demo4");
+    paragraph.textContent = "Soldatii ajung la plajele Gold si Sword, si putin mai tarziu in Juno.";
+   
+}
+function Button5(){
+    var paragraph = document.getElementById("demo5");
+    paragraph.textContent = "Soldatii avanseaza pe plaje.";
+    
+
+}
+function Button6(){
+    var paragraph = document.getElementById("demo6");
+    paragraph.textContent = "Nemtii incearca o cotra-ofesniva dar esueaza.";
+    
+
+}
+function Button7(){
+    var paragraph = document.getElementById("demo7");
+    paragraph.textContent = "Ziua-Z s-a incheiat.";
+    
+
+}
